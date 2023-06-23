9b0c506307e61c6f5b8089c56aeee996229ff73f
TheG450 <120777134+TheG450@users.noreply.github.com>
2023-06-23 14:18:16 +0700

    Update Paid.lua

--- Paid.lua
+++ Paid.lua
@@ -362,11 +362,13 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/miroe
  -----------------------------------------------------------------------------------------------------
     spawn(function()
 	    while wait(5) do
-            local anti = game:GetService("VirtualUser")
-            game:GetService("Players").LocalPlayer.Idled:connect(function()
-            anti:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-            wait(1)
-            anti:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-            end)
+            local OldNameCall = nil
+	OldNameCall = hookmetamethod(game,"__namecall",function(...)
+	   local self, key = ...
+	   if self == game:GetService("Players").LocalPlayer and getcallingmethod() == "Idled" then
+	       return true
+	   end
+	   return OldNameCall(...)
+	end)
         end
     end)
