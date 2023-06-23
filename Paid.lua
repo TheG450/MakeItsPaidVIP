be91c2969305b5926774c88abd94bcd67529d397
TheG450 <120777134+TheG450@users.noreply.github.com>
2023-06-23 10:46:39 +0700

    Update Paid.lua

--- Paid.lua
+++ Paid.lua
@@ -332,3 +332,32 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/miroe
         end
     end)
     -----------------------------------------------------------------------------------------------------
+
+    spawn(function()
+        while wait() do
+            setting = settings().Network
+            local Effect = Instance.new("ColorCorrectionEffect")
+            Effect.Parent = game.Lighting
+            Effect.Saturation = -1
+            Effect.Brightness = 0
+            Effect.Contrast = 0
+            toggle = false
+            Effect.Enabled = false
+            function onKeyPress(inputObject, gameProcessedEvent)
+            if inputObject.KeyCode == Enum.KeyCode.P then
+            if toggle == false then
+            setting.IncomingReplicationLag = 1000
+            Effect.Enabled = true
+            toggle = true
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed * 3
+            else
+            setting.IncomingReplicationLag = 0
+            Effect.Enabled = false
+            toggle = false
+            game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+            end
+            end
+            end
+            game:GetService("UserInputService").InputBegan:connect(onKeyPress)
+        end
+    end)
